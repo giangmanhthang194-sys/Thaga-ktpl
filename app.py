b3e7358da0e146588ba8eb7e23b08643d51ea46d
giangmanhthang194-sys <giangmanhthang194@gmail.com>
2025-12-26 13:43:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ generation_config = {
 }
 
 model = genai.GenerativeModel(
-  model_name="gemini-1.5-flash",
+  model_name="gemini-pro",
   generation_config=generation_config,
   system_instruction=system_instruction,
 )
